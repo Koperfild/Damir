8fc31051508574cbb52220a79ba3b99a38c192c2
Koperfild <Avalancher2@rambler.ru>
2017-03-05 22:19:16 +0300

    Немного исправлений. Добавил комментарии.

--- lessonHome1/Program.cs
+++ lessonHome1/Program.cs
@@ -9,38 +9,37 @@ namespace lessonHome1
     class Program
     {
         static void Main(string[] args)
-      {
-      
-            Shape Star1= new Shape;      
-            Rectangle Star2= new Rectangle;
-                
+        {
+            //Для вызова конструктора по умолчанию надо писать тоже (). Будет new Shape();
+            Shape Star1 = new Shape;
+            Rectangle Star2 = new Rectangle;
 
+            //Для вызова методов без параметра надо писать после него (). Т.е. будет Star1.Draw()
             Console.WriteLine(Star1.Draw);
             Console.WriteLine(Star2.Draw);
         }
+    }
+    public abstract class Shape
+    {
+        public double Square;
+        public double Perimetr;
+        public double Color;
 
-            abstract class Shape
-      {
-          public double Square;
-          public double Perimetr;
-          public double Color;
-          public void Draw();
 
-      }
-        public void Draw();
+        public void Draw()
         {
-        Perimetr=Square+4;
+            Perimetr = Square + 4;
         }
-        public class Rectangle : Shape
+    }
+    public class Rectangle : Shape
+    {
+        public int side;
+        public int bisector;
+        public int point;
+
+        public void Draw()
         {
-        public:
-        int side;
-        int bisector;
-        int point;    
-        
-        public void Draw();
-        {
-        Perimetr=Square+2;
+            Perimetr = Square + 2;
         }
     }
 }
